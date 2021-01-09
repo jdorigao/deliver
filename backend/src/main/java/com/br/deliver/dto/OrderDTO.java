83720c41be98d4262cb48dcc5696911b65c1d7b5
Juliano Dorigão <jdorigao@gmail.com>
2021-01-09 09:07:25 -0400

    Fix total

--- backend/src/main/java/com/br/deliver/dto/OrderDTO.java
+++ backend/src/main/java/com/br/deliver/dto/OrderDTO.java
@@ -16,19 +16,21 @@ public class OrderDTO implements Serializable {
     private Double longitude;
     private Instant moment;
     private OrderStatus status;
+    private Double total;
 
     private List<ProductDTO> products;
 
     public OrderDTO() {
     }
 
-    public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+    public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status, Double total) {
         this.id = id;
         this.address = address;
         this.latitude = latitude;
         this.longitude = longitude;
         this.moment = moment;
         this.status = status;
+        this.total = total;
     }
 
     public OrderDTO(Order entity) {
@@ -38,6 +40,7 @@ public class OrderDTO implements Serializable {
         longitude = entity.getLongitude();
         moment = entity.getMoment();
         status = entity.getStatus();
+        total = entity.getTotal();
         products = entity.getProducts().stream().map(ProductDTO::new).collect(Collectors.toList());
     }
 
@@ -87,6 +90,10 @@ public class OrderDTO implements Serializable {
 
     public void setStatus(OrderStatus status) {
         this.status = status;
+    }
+
+    public Double getTotal() {
+        return total;
     }
 
     public List<ProductDTO> getProducts() {
